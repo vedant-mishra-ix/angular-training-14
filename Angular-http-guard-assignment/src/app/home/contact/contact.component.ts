b5a7abf5b50806ca72ac64bb6728ab76dbab6a50
unknown <mishravedant040@gmail.com>
2022-07-07 15:14:35 +0530

    Updated Angular-http-guard-assignment

--- Angular-http-guard-assignment/src/app/home/contact/contact.component.ts
+++ Angular-http-guard-assignment/src/app/home/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormControl,FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
+  contactDetails:FormGroup = new FormGroup({});
+  constructor(private fb:FormBuilder) { }
   ngOnInit(): void {
+    this.contactDetails = this.fb.group(
+      {
+       email:['',[Validators.required]],
+       password:['',[Validators.required]],
+       confirmPassword:['',[Validators.required]],
+      }
+    )
+  }
+  Submit()
+  {
+    console.log("Submit Succesfull:", this.contactDetails.value)
+  }
+  get contact()
+  {
+    return this.contactDetails.controls;
   }
 
 }
